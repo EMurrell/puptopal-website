11914d76df2f6c87d94733c9f936c5f47246c902
EMurrell <82235988+EMurrell@users.noreply.github.com>
2021-07-08 10:28:06 -0400

    fix img for netlify and fix typo

---
+++ components/Head.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+export default function Head1() {
+  return (
+    <Head>
+      <title>Pup To Pal</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <link rel="icon" href="/bwLogo.png" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,18 @@
-import Head from "next/head";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
-import Image from "next/image";
+
 import About from "../components/About";
 import Service from "../components/Service";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Testimonials from "../components/Testimonials";
 import ContactUs from "../components/ContactUs";
+import Head from "../components/Head";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Pup To Pal</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/bwLogo.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head />
       <Navbar />
       <Header />
 
